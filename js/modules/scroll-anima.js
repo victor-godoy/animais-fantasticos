eb5d97f64cfffa8e1509f3b6a24581b76ac8a631
victor-godoy <bolanaredeb@gmail.com>
2023-08-01 18:42:17 -0300

    Scroll-anima refatorando com classe

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,30 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - this.windowMetade < 0;
+
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo");
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll(); // ele já vai iniciar com a animacao
+
+    window.addEventListener("scroll", () => {
+      this.animaScroll();
+      // o this dentro do callback tem que fazer referencia ao window por isso o (bind)
+    });
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import ScrollAnima from "./modules/scroll-anima.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
@@ -25,8 +25,6 @@ const tabNav = new TabNav(
 );
 tabNav.init();
 
-initAnimacaoScroll();
-
 // modal
 const modal = new Modal(
   '[data-modal="abrir"]',
@@ -38,6 +36,10 @@ modal.init();
 // tooltip
 const tooltip = new Tooltip("[data-tooltip]");
 tooltip.init();
+
+// animacao ao scroll
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
 
 initDropdownMenu();
 initMenuMobile();
