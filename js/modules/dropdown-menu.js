a5a51e3c43743a9c331841f74db49a6b4e765c4e
victor-godoy <bolanaredeb@gmail.com>
2023-08-03 16:46:17 -0300

    dropdownMenu refatorado

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,19 +1,52 @@
-import outsideClick from './outsideclick.js';
+import outsideClick from "./outsideclick.js";
 
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropdownMenu {
+  constructor(dropdownMenus, events) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
 
-  function handleClick(event) {
+    // define touchstart e click como argumento padrao
+    // de events caso o usuario nao defina
+    if (this.events === undefined) {
+      this.events = ["touchstart", "click"];
+    } else {
+      this.events = events;
+    }
+
+    this.activeClass = "active";
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
+
+  // ativa o dropdownMenu e adiciona o clique fora dele
+  activeDropdownMenu(event) {
     event.preventDefault();
-    this.classList.add('active');
-    outsideClick(this, ['touchstart', 'click'], () => {
-      this.classList.remove('active');
+
+    const element = event.currentTarget;
+
+    element.classList.add(this.activeClass);
+    outsideClick(element, this.events, () => {
+      // estamos usando o this aqui, mas se vermos o outsideClick lá verificamos se estou
+      // clicando dentro do elemento ou fora dele
+      // e meu elemento não é this( faz referencia ao objeto)
+      // então temos que mudar isso para ( event.currentTarget ) e vou passar ele em uma constante
+
+      element.classList.remove(this.activeClass);
     });
   }
 
-  dropdownMenus.forEach((menu) => {
-    ['touchstart', 'click'].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
+  // adiciona os eventos ao dropdownMenu
+  addEventListener() {
+    this.dropdownMenus.forEach((menu) => {
+      this.events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
+      });
     });
-  });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addEventListener();
+    }
+
+    return this;
+  }
 }
